f9d7cbf016e6e0cfe3c8005ff1f16aade49578ee
han chulmin <alysion@hanui-MacBookPro.local>
2024-01-18 23:47:16 +0900

    test commit

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main() {
+
+	dynamic name; // == var name;
+    if(name is String) {
+      name.length;
+    }
+    if(name is int) {
+      name.isInfinite;
+    }
+    //test code 
+      
+  print('hello world');
+}
